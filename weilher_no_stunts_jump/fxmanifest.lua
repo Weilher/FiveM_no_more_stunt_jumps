25839fcc10b4db842af7823e4d2e34419687c3cb
Weilher <82490121+Weilher@users.noreply.github.com>
2023-08-20 17:08:51 +0200

    Add files via upload

--- weilher_no_stunts_jump/fxmanifest.lua
+++ weilher_no_stunts_jump/fxmanifest.lua
@@ -5,37 +5,3 @@ this_is_a_map "yes"
 author 'Weilher'
 description 'Removal of a lot of stunts and normal jumps on the map'
 version '1.0'
-
-/* 
-Stunts Removed :
-    Vinewood Hills :
-        #11 
-    Little Seoul :
-        #2
-        #46
-    Pillbox, Mision Row & Textile City :
-        #6
-    La Mesa :
-        #24
-    Davis & Rancho :
-        #17
-    Banning :
-        #22
-    La Puerta :
-        #26
-    Los Santos International Airport :
-        #27
-    Port of South LS : 
-        #31
-        #34
-        #35
-        #36
-        #37
-        #38
-        #39
-        #40
-
-Known bugs :
-    colisions still here on #24 stunt jump
-
-*/
