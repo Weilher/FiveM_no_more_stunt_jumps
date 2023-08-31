e905e3d44b2fdab02918106d3d65a0ef2b4d5b18
Weilher <82490121+Weilher@users.noreply.github.com>
2023-08-31 18:37:47 +0200

    1.2 update

--- weilher_no_stunts_jump/fxmanifest.lua
+++ weilher_no_stunts_jump/fxmanifest.lua
@@ -1,9 +1,8 @@
 fx_version "cerulean"
 game { "gta5" }
 this_is_a_map "yes"
-
 author "Weilher"
 description "Removal of a lot of stunts and normal jumps on the map"
-version "1.1"
+version "1.2"
 
 server_script "server/check_version.lua"
